e90ee65f4c32421849494c2df31a2567041e861e
Carmine Scarpitta <carmine.scarpitta@uniroma2.it>
2022-09-05 17:54:14 +0200

    zebra: add support for generic netlink
    Signed-off-by: Carmine Scarpitta <carmine.scarpitta@uniroma2.it>

---
+++ zebra/ge_netlink.h
@@ -0,0 +1,40 @@
+/* Header file exported by ge_netlink.c to zebra.
+ * Copyright (C) 2022  Carmine Scarpitta, University of Rome Tor Vergata
+ *
+ * This file is part of GNU Zebra.
+ *
+ * GNU Zebra is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2, or (at your option) any
+ * later version.
+ *
+ * GNU Zebra is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; see the file COPYING; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+#ifndef _ZEBRA_GE_NETLINK_H
+#define _ZEBRA_GE_NETLINK_H
+
+#include "zebra_dplane.h"
+
+#ifdef HAVE_NETLINK
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+extern int genl_resolve_family(const char *family, struct zebra_dplane_ctx *ctx);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* HAVE_NETLINK */
+
+#endif /* _ZEBRA_GE_NETLINK_H */
