5eae15a4591d3e6fc35cac7d4919d53155442fd7
Carmine Scarpitta <carmine.scarpitta@uniroma2.it>
2022-11-03 16:24:52 +0100

    zebra: Add dplane op for SRv6 encap source address
    In this commit, we add a new data plane operation that is used to
    configure the SRv6 encapsulation source address in the data plane.
    
    Signed-off-by: Carmine Scarpitta <carmine.scarpitta@uniroma2.it>

--- zebra/ge_netlink.h
+++ zebra/ge_netlink.h
@@ -21,6 +21,8 @@
 #ifndef _ZEBRA_GE_NETLINK_H
 #define _ZEBRA_GE_NETLINK_H
 
+#include <linux/genetlink.h>
+
 #include "zebra_dplane.h"
 
 #ifdef HAVE_NETLINK
@@ -29,8 +31,24 @@
 extern "C" {
 #endif
 
+/* Generic Netlink request message */
+struct genl_request {
+	struct nlmsghdr n;
+	struct genlmsghdr g;
+	char buf[1024];
+};
+
 extern int genl_resolve_family(const char *family,
 			       struct zebra_dplane_ctx *ctx);
+extern ssize_t netlink_sr_tunsrc_set_msg_encode(int cmd,
+						struct zebra_dplane_ctx *ctx,
+						void *buf, size_t buflen);
+extern ssize_t netlink_sr_tunsrc_set_msg_encoder(struct zebra_dplane_ctx *ctx,
+						 void *buf, size_t buflen);
+struct nl_batch;
+extern enum netlink_msg_status
+netlink_put_sr_tunsrc_set_msg(struct nl_batch *bth,
+			      struct zebra_dplane_ctx *ctx);
 
 #ifdef __cplusplus
 }
