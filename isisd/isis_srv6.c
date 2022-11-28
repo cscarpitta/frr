bc9125b50adf64014b14dd2e7a826eca01d3a222
Carmine Scarpitta <carmine.scarpitta@uniroma2.it>
2022-11-28 10:34:10 +0100

    isisd: Add isis_srv6.[c, h] files
    Signed-off-by: Carmine Scarpitta <carmine.scarpitta@uniroma2.it>

---
+++ isisd/isis_srv6.c
@@ -0,0 +1,25 @@
+/*
+ * This is an implementation of Segment Routing over IPv6 (SRv6) for IS-IS
+ * as per draft-ietf-lsr-isis-srv6-extensions
+ * https://datatracker.ietf.org/doc/html/draft-ietf-lsr-isis-srv6-extensions
+ *
+ * Copyright (C) 2022 University of Rome Tor Vergata
+ *
+ * Author: Carmine Scarpitta <carmine.scarpitta@uniroma2.it>
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 2 of the License, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; see the file COPYING; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+#include <zebra.h>
