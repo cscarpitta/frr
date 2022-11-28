9330ea778ef4c50b759c731cfaa22b9ea4e4ff43
Carmine Scarpitta <carmine.scarpitta@uniroma2.it>
2022-11-28 11:31:58 +0100

    isisd: Add YANG node to enable SRv6
    Signed-off-by: Carmine Scarpitta <carmine.scarpitta@uniroma2.it>

--- isisd/isis_nb.h
+++ isisd/isis_nb.h
@@ -259,6 +259,8 @@ int isis_instance_segment_routing_prefix_sid_map_prefix_sid_last_hop_behavior_mo
 	struct nb_cb_modify_args *args);
 int isis_instance_segment_routing_prefix_sid_map_prefix_sid_n_flag_clear_modify(
 	struct nb_cb_modify_args *args);
+int isis_instance_segment_routing_srv6_enabled_modify(
+	struct nb_cb_modify_args *args);
 int isis_instance_mpls_ldp_sync_destroy(struct nb_cb_destroy_args *args);
 int isis_instance_mpls_ldp_sync_create(struct nb_cb_create_args *args);
 int isis_instance_mpls_ldp_sync_holddown_modify(struct nb_cb_modify_args *args);
