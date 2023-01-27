58f57f5d416ce3d405c3112377ed591e250d063d
Carmine Scarpitta <carmine.scarpitta@uniroma2.it>
2023-01-27 13:26:54 +0100

    isisd: Add IS-IS SRv6 End SID Sub-TLV Codepoint
    Add the `IS-IS SRv6 End SID Sub-TLV Codepoint` as defined in the
    `IS-IS Sub-TLVs for TLVs Advertising Prefix Reachability` IANA registry.
    This codepoint will be used as the Sub-TLV
    Type to advertise the SRv6 End SID Sub-TLV in the SRv6 Locator TLV.
    
    Signed-off-by: Carmine Scarpitta <carmine.scarpitta@uniroma2.it>

--- isisd/isis_tlvs.h
+++ isisd/isis_tlvs.h
@@ -474,6 +474,8 @@ enum isis_tlv_type {
 	/* draft-ietf-lsr-isis-srv6-extensions section #4.4 */
 	ISIS_SUBTLV_SRV6_MAX_END_D_MSD = 45,
 
+	ISIS_SUBTLV_SRV6_END_SID = 5,
+
 	/* draft-ietf-lsr-isis-srv6-extensions */
 	ISIS_SUBSUBTLV_SID_STRUCTURE = 1,
 
