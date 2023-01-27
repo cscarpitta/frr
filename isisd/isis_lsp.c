a8ac6229bd70b1c25f8a63928b63bc3782e30430
Carmine Scarpitta <carmine.scarpitta@uniroma2.it>
2023-01-27 11:16:07 +0100

    fix add locator tlv to lsp

--- isisd/isis_lsp.c
+++ isisd/isis_lsp.c
@@ -1170,10 +1170,10 @@ static void lsp_build(struct isis_lsp *lsp, struct isis_area *area)
 			  area->area_tag);
 
 
-		isis_tlvs_add_ipv6_reach(
-			lsp->tlvs,
-			isis_area_ipv6_topology(area), &chunk->prefix,
-			222, false, NULL);
+		// isis_tlvs_add_ipv6_reach(
+		// 	lsp->tlvs,
+		// 	isis_area_ipv6_topology(area), &chunk->prefix,
+		// 	222, false, NULL);
 	}
 
 	/* IPv4 address and TE router ID TLVs.
