d4e3f37f69a60dac806b858aace8bb97c933bbeb
Carmine Scarpitta <carmine.scarpitta@uniroma2.it>
2023-02-09 19:42:18 +0100

    add srv6 locator tlv data structure

--- isisd/isis_tlvs.h
+++ isisd/isis_tlvs.h
@@ -206,6 +206,16 @@ struct isis_lan_adj_sid {
 #define MSD_TYPE_BASE_MPLS_IMPOSITION  0x01
 #define MSD_TLV_SIZE            2
 
+/* draft-ietf-lsr-isis-srv6-extensions-19 section 7.1 */
+struct isis_srv6_locator {
+	uint32_t metric;
+	uint8_t flags;
+	uint8_t algorithm;
+	struct prefix_ipv6 locator;
+};
+
+#define ISIS_SRV6_LOCATOR_HDR_SIZE 22
+
 /* Maximum SRv6 SID Depths (MSD) as per draft-ietf-lsr-isis-srv6-extensions-19
  * section #4 */
 struct isis_srv6_msd {
@@ -347,6 +357,7 @@ struct isis_tlvs {
 	struct isis_threeway_adj *threeway_adj;
 	struct isis_router_cap *router_cap;
 	struct isis_spine_leaf *spine_leaf;
+	struct isis_srv6_locator *srv6_locator;
 };
 
 enum isis_tlv_context {
