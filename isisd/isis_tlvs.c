77d62e09510dda00a36f5edc40ffc2a4a204675d
Carmine Scarpitta <carmine.scarpitta@uniroma2.it>
2023-02-14 16:03:58 +0100

    isisd: Add pack func for SID Structure Sub-Sub-TLV
    Add a function to pack an SRv6 SID Structure Sub-Sub-TLV
    (RFC 9352 section #9).
    
    Signed-off-by: Carmine Scarpitta <carmine.scarpitta@uniroma2.it>

--- isisd/isis_tlvs.c
+++ isisd/isis_tlvs.c
@@ -5191,6 +5191,26 @@ static void free_subsubtlv_srv6_sid_structure(
 	XFREE(MTYPE_ISIS_SUBSUBTLV, sid_struct);
 }
 
+static int pack_subsubtlv_srv6_sid_structure(
+	struct isis_srv6_sid_structure_subsubtlv *sid_struct, struct stream *s)
+{
+	if (!sid_struct)
+		return NULL;
+
+	if (STREAM_WRITEABLE(s) < 6) {
+		return 1;
+	}
+
+	stream_putc(s, ISIS_SUBSUBTLV_SRV6_SID_STRUCTURE);
+	stream_putc(s, 4);
+	stream_putc(s, sid_struct->loc_block_len);
+	stream_putc(s, sid_struct->loc_node_len);
+	stream_putc(s, sid_struct->func_len);
+	stream_putc(s, sid_struct->arg_len);
+
+	return 0;
+}
+
 /* Functions related to tlvs in general */
 
 struct isis_tlvs *isis_alloc_tlvs(void)
