769a6c92b112cf4ee3393403845169a459427e70
Carmine Scarpitta <carmine.scarpitta@uniroma2.it>
2023-02-14 13:30:37 +0100

    copy_item_srv6_end_sid
    Signed-off-by: Carmine Scarpitta <carmine.scarpitta@uniroma2.it>

--- isisd/isis_tlvs.c
+++ isisd/isis_tlvs.c
@@ -1419,6 +1419,19 @@ static int unpack_subtlv_ipv6_source_prefix(enum isis_tlv_context context,
 	return 0;
 }
 
+/* Functions for Sub-TLV 5 SRv6 End SID as per draft-ietf-lsr-isis-srv6-extensions section 7.2 */
+static struct isis_item *copy_item_srv6_end_sid(struct isis_item *i)
+{
+	struct isis_srv6_end_sid_subtlv *sid = (struct isis_srv6_end_sid_subtlv *)i;
+	struct isis_srv6_end_sid_subtlv *rv = XCALLOC(MTYPE_ISIS_SUBTLV, sizeof(*rv));
+
+	rv->behavior = sid->behavior;
+	rv->value = sid->value;
+	rv->subsubtlvs = copy_subsubtlvs(sid->subsubtlvs);
+
+	return (struct isis_item *)rv;
+}
+
 static struct isis_item *copy_item(enum isis_tlv_context context,
 				   enum isis_tlv_type type,
 				   struct isis_item *item);
