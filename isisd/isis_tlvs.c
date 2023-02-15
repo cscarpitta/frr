eba87797fcb3cbea9e401ed99da859e87cdddc15
Carmine Scarpitta <carmine.scarpitta@uniroma2.it>
2023-02-15 12:43:59 +0100

    remove old isis_tlvs_set_srv6_locator
    Signed-off-by: Carmine Scarpitta <carmine.scarpitta@uniroma2.it>

--- isisd/isis_tlvs.c
+++ isisd/isis_tlvs.c
@@ -7103,18 +7103,6 @@ void isis_tlvs_set_purge_originator(struct isis_tlvs *tlvs,
 	}
 }
 
-/* Set SRv6 Locator TLV parameters */
-void isis_tlvs_set_srv6_locator(struct isis_tlvs *tlvs,
-				     const struct isis_srv6_locator *locator)
-{
-	XFREE(MTYPE_ISIS_TLV, tlvs->srv6_locator);
-	if (!locator)
-		return;
-
-	tlvs->srv6_locator = XCALLOC(MTYPE_ISIS_TLV, sizeof(*tlvs->srv6_locator));
-	*tlvs->srv6_locator = *locator;
-}
-
 /* Set SRv6 SID Structure Sub-Sub-TLV parameters */
 void isis_subsubtlvs_set_srv6_sid_structure(struct isis_subsubtlvs *subsubtlvs,
 					    struct isis_srv6_sid *sid)
