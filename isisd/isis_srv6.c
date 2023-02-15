d5f4717fe9983d2c59e6b7cbecb1f5778d016e45
Carmine Scarpitta <carmine.scarpitta@uniroma2.it>
2023-02-15 11:43:03 +0100

    fix isis_srv6_end_sid2subtlv
    Signed-off-by: Carmine Scarpitta <carmine.scarpitta@uniroma2.it>

--- isisd/isis_srv6.c
+++ isisd/isis_srv6.c
@@ -37,6 +37,25 @@
 DEFINE_MTYPE_STATIC(ISISD, ISIS_SRV6_SID, "ISIS SRv6 Segment ID");
 
 /**
+ * Fill in SRv6 End SID Sub-TLV according to the corresponding configuration.
+ *
+ * @param sid	      SRv6 End SID configuration
+ * @param sid_subtlv  SRv6 End SID Sub-TLV to be updated
+ */
+void isis_srv6_end_sid2subtlv(const struct isis_srv6_sid *sid,
+			      struct isis_srv6_end_sid_subtlv *sid_subtlv)
+{
+	/* Set SRv6 End SID flags */
+	sid_subtlv->flags = sid->flags;
+
+	/* Set SRv6 EndSID behavior */
+	sid_subtlv->behavior = sid->behavior;
+
+	/* Set SRv6 End SID value */
+	sid_subtlv->value = sid->value;
+}
+
+/**
  * Fill in SRv6 Locator TLV according to the corresponding configuration.
  *
  * @param loc	     SRv6 Locator configuration
