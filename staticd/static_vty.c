4d08bcbe70cb91d6187c37e549f5406b52818b47
Carmine Scarpitta <cscarpit@cisco.com>
2024-12-24 08:59:18 +0100

    staticd: Add `segments` keyword to `ip_route_vrf` CLI
    Add `segments` keyword to `ip_route_vrf` CLI to support steering of
    IPv4 routes over an SRv6 policy.
    
    Signed-off-by: Carmine Scarpitta <cscarpit@cisco.com>

--- staticd/static_vty.c
+++ staticd/static_vty.c
@@ -829,6 +829,7 @@ DEFPY_YANG(ip_route_vrf,
 	  |nexthop-vrf NAME                            \
 	  |color (1-4294967295)                        \
 	  |bfd$bfd [{multi-hop$bfd_multi_hop|source A.B.C.D$bfd_source|profile BFDPROF$bfd_profile}] \
+	  |segments WORD \
           }]",
       NO_STR IP_STR
       "Establish static routes\n"
@@ -852,7 +853,9 @@ DEFPY_YANG(ip_route_vrf,
       BFD_INTEGRATION_SOURCE_STR
       BFD_INTEGRATION_SOURCEV4_STR
       BFD_PROFILE_STR
-      BFD_PROFILE_NAME_STR)
+      BFD_PROFILE_NAME_STR
+      "Value of segs\n"
+      "Segs (SIDs)\n")
 {
 	struct static_route_args args = {
 		.delete = !!no,
@@ -873,6 +876,7 @@ DEFPY_YANG(ip_route_vrf,
 		.bfd_multi_hop = !!bfd_multi_hop,
 		.bfd_source = bfd_source_str,
 		.bfd_profile = bfd_profile,
+		.segs = segments,
 	};
 
 	return static_route_nb_run(vty, &args);
